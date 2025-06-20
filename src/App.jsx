ed36acac65aa0e467042cad77dc3cf1998578366
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2025-06-20 17:09:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import "./App.css";
 import ChatApp from "./components/ChatApp/ChatApp.jsx";
-import { useEffect, useState } from "react";
+
 
 function App() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
+ 
   return (
     <>
-      <header>
-        <div>Time: {time}</div>
-      </header>
       <main>
         <ChatApp />
       </main>
