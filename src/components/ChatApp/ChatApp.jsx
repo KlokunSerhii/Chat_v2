2ce51cd5329930fd82e79fec86220c1521287707
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2025-06-21 16:22:17 +0300

    Update ChatApp.jsx

--- src/components/ChatApp/ChatApp.jsx
+++ src/components/ChatApp/ChatApp.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
-import Picker from "@emoji-mart/react";
+import EmojiPicker from "emoji-picker-react";
 import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
 import {
   ChatContainer,
   ChatHeader,
