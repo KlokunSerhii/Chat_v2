0050399e0e679cc38f9b597d50f76cfbcd43cfed
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2025-07-02 16:36:33 +0300

    Update MessageItem.jsx

--- src/components/MessageItem/MessageItem.jsx
+++ src/components/MessageItem/MessageItem.jsx
@@ -176,7 +176,10 @@ export default function MessageItem({
                       borderTopRightRadius: '8px',
                     }}
                   />
-                )}
+                ):(
+  <div style={{ padding: 8, fontSize: 12, color: "#999" }}>
+    Зображення недоступне
+  </div>))}
                 <div style={{ padding: '8px' }}>
                   <strong style={{ fontSize: '14px' }}>{msg.linkPreview.title}</strong>
                   <p style={{ fontSize: '12px', color: '#666' }}>{msg.linkPreview.description}</p>
@@ -184,10 +187,7 @@ export default function MessageItem({
                 </div>
               </a>
             </div>
-          ):(
-  <div style={{ padding: 8, fontSize: 12, color: "#999" }}>
-    Зображення недоступне
-  </div>)}
+          )}
           {msg.image && (
             <FileLabelContainer>
               <MessageImage
